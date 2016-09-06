8cffce0a8a9a7d393643f665f3c2a48b965d8a0d
Russell Vea <russellbvea@gmail.com>
2016-09-05 21:53:58 -1000

    Searchable vendors.

--- wp-content/themes/madeinhawaii/search.php
+++ wp-content/themes/madeinhawaii/search.php
@@ -8,16 +8,21 @@
  * @subpackage  Timber
  * @since   Timber 0.1
  */
+ global $wpdb;
 
 $templates = array( 'search.twig', 'archive.twig', 'index.twig' );
 $context = Timber::get_context();
 
+$search = $_GET['s'];
 $context['title'] = 'Search results for &quot;'. get_search_query() . "&quot;";
 $products =
   Timber::get_posts(
-    ['post_type' => 'product', 's' => $_GET['s']]
+    ['post_type' => 'product', 's' => $search]
   );
 
 $context['products'] = $products;
+
+$users = $wpdb->get_results("SELECT * from $wpdb->users WHERE display_name LIKE \"%{$search}%\";");
+$context['users'] = $users;
 
 Timber::render( $templates, $context );
